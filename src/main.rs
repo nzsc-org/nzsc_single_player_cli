e3bd9d4824fa3afa5b4afff49a3cad09169166c4
Kyle Lin <kylejlin.us@gmail.com>
2018-05-13 21:24:36 -0700

    Ask if user wants to play again

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 mod nzsc;
 
+use std::io;
+
 fn main() {
-    nzsc::single_player_game::start();
+    loop {
+        nzsc::single_player_game::start();
+
+        let mut response = String::new();
+        println!("Play again? y/N");
+        io::stdin().read_line(&mut response)
+            .expect("Failed to read line.");
+
+        if response.trim().to_lowercase().chars().next().unwrap() != 'y' {
+            break;
+        }
+    }
 }
